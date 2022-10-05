5ef6a7f28862d2b8bdb43daf0ff002fae5b4313d
Ifcodes <ifeco133@gmail.com>
2022-10-05 18:21:29 +0100

    feat: authContext-line 16 implementation

--- src/authContext.jsx
+++ src/authContext.jsx
@@ -15,9 +15,14 @@ const reducer = (state, action) => {
   switch (action.type) {
     case "LOGIN":
       //TODO
-
+      localStorage.setItem("token", action.payload.token);
+      localStorage.setItem("role", action.payload.role);
       return {
         ...state,
+        isAuthenticated: true,
+        user: action.payload.user_id,
+        token: action.payload.token,
+        role: action.payload.role,
       };
     case "LOGOUT":
       localStorage.clear();
